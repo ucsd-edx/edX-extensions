f786cf63c032c58bb6ca3c7214d84625c6c7e3b3
Ubuntu <ubuntu@ip-172-31-7-147.us-west-2.compute.internal>
2017-05-28 00:17:53 +0000

    incorporate mount system

--- edX-nbgrader/docker_grader.py
+++ edX-nbgrader/docker_grader.py
@@ -4,6 +4,9 @@ import shutil
 import os
 import json
 
+# DOCKER_IMAGE = 'zhipengyan/ucsdx:docker-grader'
+DOCKER_IMAGE = 'zhipengyan/ucsdx:test'
+COURSE_VOLUME = {'/home/ubuntu/edX-extensions/edX-nbgrader/dummy-course': {'bind': '/app/dummy-course', 'mode': 'rw'}}
 
 class DockerGrader(Grader):
     """
@@ -17,11 +20,18 @@ class DockerGrader(Grader):
 
         command_str = 'python2.7 grade.py {} {} {}'.format(section_name, problem_name, submission_url)
         c = docker.from_env()
-        results = c.containers.run('zhipengyan/ucsdx:docker-grader', remove=True, command=command_str)
-        results = eval(results)
+        results = c.containers.run(DOCKER_IMAGE, remove=True, command=command_str, volumes=COURSE_VOLUME)
+        try:
+            results = eval(results)
+        except Exception as e:
+            results = json.loads(results)
         results.update({'tests':'', 'errors':''})
         return results
 
 if __name__ == "__main__":
     d = DockerGrader()
-    print d.grade('ps1/problem1', 'http://asdf.com', '')
+    sample_nb = 'https://raw.githubusercontent.com/jupyter/nbgrader/master/nbgrader/docs/source/user_guide/source/ps1/problem1.ipynb'
+    print '********* dummy test *********'
+    print d.grade('ps1/problem1', '{"problem1.ipynb": "http://asdf.com"}', '')
+    print '********* grading test *********'
+    print d.grade('ps1/problem1', '{{"problem1.ipynb": "{}"}}'.format(sample_nb), '')
