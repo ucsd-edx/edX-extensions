6aaa4ccf768db12e725fcca82dfa4f1738903c45
Zhen Zhai <zzhai@eng.ucsd.edu>
2017-07-11 14:17:21 -0700

    cleaned up translator class

--- edX-markdown/TranslaterClass.py
+++ edX-markdown/TranslaterClass.py
@@ -52,32 +52,12 @@ class Translator:
     input_file   =None
     output_file  =None
     
-    imd          =None
     py_code      =None
     md_code      =None
     test_code    =None
     html_code    =None
     xml_code     =None
-    
-    """
-    Helper Functions
-    """
-    def read_imd(self):
-        f = open(self.input_file, "r")
-        contents = f.readlines()
-        f.close()
-        return contents
 
-    def write_xml(self):
-        f = open(self.output_file, "w")
-        f.write(self.xml_code)
-        f.close()
-           
-    def toHtml(self):
-        return markdown.markdown("".join(self.md_code),extensions=['markdown.extensions.tables'],output_format="HTML")
-
-    
-    
     
     """
     The translator class translates IMD files, into other formats.     
@@ -104,267 +84,240 @@ class Translator:
         file_name = mapping[mapping_key]
         
         self.input_file  = input_dir+"/{0}".format(file_name)
-        self.imd         = self.read_imd( ) 
         self.output_file = output_dir+"/{0}.xml".format(mapping_key) 
 
+    """
+    Private wrappers
 
-# # <span style="color:blue">Function: </span> XML Wrappers
+        The following functions are all helper functions for the Translator class.
+        They are used when code is being converted into the XML format.
+        Basically, what these functions do is provide the correct xml format
+        when converting specifics parts of the `.imd` into `.xml`
+    """
 
-# The following functions are all helper functions for the Translator class. They are used when code is being converted into the XML format. Basically, what these functions do is provide the correct xml format when converting specifics parts of the `.imd` into `.xml`
+    def __XML_wrapper(self, content):
+        return '<problem>\n  <text>\n{0}\n  </text>\n</problem>\n'.format(content)
 
-# In[3]:
+    def __py_wrapper(self, code):
+        #XML_PY_EVAL = XML_PY_START + 'from hint import evaluate\ndef check(expect, ans):\n  return evaluate.evaluate(expect, ans)\n' + XML_PY_END
+        return '    <script type="loncapa/python">\n{0}\n    </script>\n\n\n'.format(code)
 
-def XML_wrapper(self, content):
-    return '<problem>\n  <text>\n{0}\n  </text>\n</problem>\n'.format(content)
+    def __math_wrapper(self, sol):
+        return '    <customresponse cfn="check" expect="\[${0}\]">\n      <textline/>\n    </customresponse>\n\n'.format(sol)
 
-Translator.XML_wrapper = XML_wrapper
+    def __option_wrapper(self, opt, sol):
+        opt_string = '      <optioninput options="${0}" correct="${1}"/>'.format(opt, sol)
+        return '    <optionresponse>\n'+opt_string+'\n    </optionresponse>\n\n'
 
+    def __multi_choice_wrapper(self, choices):
+        head = '    <choiceresponse>\n      <checkboxgroup>\n'
+        end = '      </checkboxgroup>\n    </choiceresponse>\n\n'
+        return head+choices+end
 
-# In[4]:
+    def __correct_choice_wrapper(self, choice):
+        return '<choice correct="true">{0}</choice>\n'.format(choice)
 
-def py_wrapper(self, code):
-    #XML_PY_EVAL = XML_PY_START + 'from hint import evaluate\ndef check(expect, ans):\n  return evaluate.evaluate(expect, ans)\n' + XML_PY_END
-    return '    <script type="loncapa/python">\n{0}\n    </script>\n\n\n'.format(code)
-
-Translator.py_wrapper = py_wrapper
-
-
-# In[5]:
-
-def math_wrapper(self, sol):
-    return '    <customresponse cfn="check" expect="\[${0}\]">\n      <textline/>\n    </customresponse>\n\n'.format(sol)
-
-Translator.math_wrapper = math_wrapper
-
-
-# In[6]:
-
-def option_wrapper(self, opt, sol):
-    opt_string = '      <optioninput options="${0}" correct="${1}"/>'.format(opt, sol)
-    return '    <optionresponse>\n'+opt_string+'\n    </optionresponse>\n\n'
-
-Translator.option_wrapper = option_wrapper
+    def __wrong_choice_wrapper(self, choice):
+        return '<choice correct="false">{0}</choice>\n'.format(choice)
 
 
-# In[7]:
+    """
+    Public Functions
+    """
+    def read_imd(self):
+        f = open(self.input_file, "r")
+        contents = f.readlines()
+        f.close()
+        return contents
 
-def multi_choice_wrapper(self, choices):
-    head = '    <choiceresponse>\n      <checkboxgroup>\n'
-    end = '      </checkboxgroup>\n    </choiceresponse>\n\n'
-    return head+choices+end
-
-Translator.multi_choice_wrapper = multi_choice_wrapper
-
-
-# In[8]:
-
-def correct_choice_wrapper(self, choice):
-    return '<choice correct="true">{0}</choice>\n'.format(choice)
-
-Translator.correct_choice_wrapper = correct_choice_wrapper
-
-
-# In[9]:
-
-def wrong_choice_wrapper(self, choice):
-    return '<choice correct="false">{0}</choice>\n'.format(choice)
-
-Translator.wrong_choice_wrapper = wrong_choice_wrapper
+    def write_xml(self):
+        f = open(self.output_file, "w")
+        f.write(self.xml_code)
+        f.close()
+           
+    def toHtml(self):
+        return markdown.markdown("".join(self.md_code),extensions=['markdown.extensions.tables'],output_format="HTML")
 
 
-# # <span style="color:blue">Function: </span> Load Imd
-# 
+    def loadImd(self, imd_contents):
+        '''
+        Splits imd codes into the python portion, the test portion, and the true imd portion.
+        input:
+            imd codes
+        output:
+            python_code: python code
+            md_code: markdown code
+            test_code: test code
+        '''
 
-# This function loads the imd code from the input file. It then splits the imd code into the  python portion, the test portion, and the true imd portion.
-
-# In[10]:
-
-def loadImd(self):
-    '''
-    Given imd file content, split python code, markdown code, and test code.
-    input:
-        contents: imd file content
-    output:
-        python_code: python code
-        md_code: markdown code
-        test_code: test code
-    '''
-
-    contents = self.imd
-    
-    # Read python code
-    if "```python\n" in contents:
-        start_index = contents.index("```python\n")
-        end_index = contents.index("```\n")
-        python_code = contents[start_index+1:end_index]
-        python_code = "".join(python_code)
-        contents = contents[:start_index] + contents[end_index+1:]
-    else:
-        python_code = ""
-
-    # Read test code
-    if "```test\n" in contents:
-        start_index = contents.index("```test\n")
-        if "```\n" in contents:
-            end_index = contents.index("```\n")
-        elif "```" in contents:
-            end_index = contents.index("```")
-        else:
-            print "didn't close test section"
-        test_code = contents[start_index+1:end_index]
-        test_code = "".join(test_code)
-        contents = contents[:start_index] + contents[end_index+1:]
-    else:
-        test_code = ""
-
-    md_code = contents[:]
-
-    inline = 0  # open or closed parantheses
-    double = 0  # open or closed brackets
-    inlineSub = ['\\\\\\(', '\\\\\\)']
-    doubleSub = ['\\\\\\[', '\\\\\\]']
-
-    # Read markdown line by line
-    for j in xrange(len(md_code)):
-        line = md_code[j]
-
-        # trim the empty space at the front
-        while line[0] == " ":
-            line = line[1:]
-
-        # trim the empty space at the end, but preserve the newline
-        while len(line) > 3 and line[-2] == " ":
-            line = line[:-2] + line[-1]
-
-        # record dollar sign in $var by a safe substitute
-        line = line.replace('\\$','\001')
-
-        # substitute inline and newline math expression wrapper
-        i=0
-        while i < len(line):
-            if line[i:i+2] == '$$':
-                line = line[:i]+doubleSub[double]+line[i+2:]
-                double = 1 - double
-                i += 4
-            elif line[i] == '$':
-                line = line[:i]+inlineSub[inline]+line[i+1:]
-                inline = 1-inline
-                i += 4
-            else:
-                i += 1
-
-        # mark variables with $ sign
-        line = line.replace('\001','$')
-        md_code[j] = line
+        contents = imd_contents
         
-    self.py_code = python_code
-    self.md_code = md_code
-    self.test_code = test_code
-    self.html_code = self.toHtml()
-
-Translator.loadImd = loadImd
-
-
-# # <span style="color:blue">Function: </span> Make XML
-
-# In[11]:
-
-def toXml(self):
-    """
-    returns the XML code that is to be pasted into EDX studio
-    """
-    html_code = self.html_code.splitlines()
-    py_code_lines = self.py_code.splitlines()
-
-    choice_list = ""
-    updated_html_code = []
-    part_id = 1
-    for line in html_code:
-        if '<p>[_choice]</p>' == line:
-            updated_html_code += ['\n', '\n']
-            for s in py_code_lines:
-                if '=' not in s:
-                    continue
-                strip_start_index = s.index('=')
-                strip_s = s[:strip_start_index+1].replace(" ", "") + s[strip_start_index+1:]
-                sol_str = 'solution'+str(part_id)+"="
-                opt_str = 'option'+str(part_id)+"="
-                if sol_str in strip_s:
-                    sol_index = strip_s.index(sol_str)
-                    sol = strip_s[sol_index+len(sol_str):]
-                    while sol[0] == ' ':
-                        sol = sol[1:]
-                    sol = sol.replace('\n','')
-                    sol = sol.replace('"','')
-                    sol = sol.replace("'", "")
-                elif opt_str in strip_s:
-                    opt_index = strip_s.index(opt_str)
-                    opt = strip_s[opt_index+len(opt_str):]
-                    while opt[0] == ' ':
-                        opt = opt[1:]
-                    opt = opt.replace("\n","")
-
-            xml_code = self.option_wrapper(opt, sol)
-            updated_html_code += xml_code
-            part_id += 1
-
-        elif '<p>[_]</p>' == line:
-            xml_code = self.math_wrapper('solution'+str(part_id))
-            updated_html_code += xml_code
-            part_id += 1
-
-        elif '[ ]' in line or '[x]' in line:
-            # trim <p> and </p>
-            if '<p>' in line:
-                updated_html_code += ['\n', '\n']
-                choice_context = line[3:]
-            if '</p>' in line:
-                end_index = line.index("</p>")
-                choice_context = line[:end_index]
-
-            while choice_context[0] == ' ':
-                choice_context = choice_context[1:]
-            while choice_context[-1] == ' ':
-                choice_context = choice_context[:-1]
-
-            if '[ ]' in line:
-                choice_insert = self.wrong_choice_wrapper(choice_context[3:])
-            elif '[x]' in line:
-                choice_insert = self.correct_choice_wrapper(choice_context[3:])
-
-            choice_list += choice_insert
-
-            if '</p>' in line:
-                updated_html_code += self.multi_choice_wrapper(choice_list)
-                part_id += 1
-                choice_list = ""
-
+        # Read python code
+        if "```python\n" in contents:
+            start_index = contents.index("```python\n")
+            end_index = contents.index("```\n")
+            python_code = contents[start_index+1:end_index]
+            python_code = "".join(python_code)
+            contents = contents[:start_index] + contents[end_index+1:]
         else:
-            updated_html_code.append(line)
-            updated_html_code.append("\n")
+            python_code = ""
 
-    updated_html_code = "".join(updated_html_code)
-    
-    """   ERROR ERROR ERROR"""
-    xml_code = self.XML_wrapper( self.py_wrapper(self.py_code) + updated_html_code)
-    self.xml_code = "".join(xml_code)
+        # Read test code
+        if "```test\n" in contents:
+            start_index = contents.index("```test\n")
+            if "```\n" in contents:
+                end_index = contents.index("```\n")
+            elif "```" in contents:
+                end_index = contents.index("```")
+            else:
+                print "didn't close test section"
+            test_code = contents[start_index+1:end_index]
+            test_code = "".join(test_code)
+            contents = contents[:start_index] + contents[end_index+1:]
+        else:
+            test_code = ""
+
+        md_code = contents[:]
+
+        inline = 0  # open or closed parantheses
+        double = 0  # open or closed brackets
+        inlineSub = ['\\\\\\(', '\\\\\\)']
+        doubleSub = ['\\\\\\[', '\\\\\\]']
+
+        # Read markdown line by line
+        for j in xrange(len(md_code)):
+            line = md_code[j]
+
+            # trim the empty space at the front
+            while line[0] == " ":
+                line = line[1:]
+
+            # trim the empty space at the end, but preserve the newline
+            while len(line) > 3 and line[-2] == " ":
+                line = line[:-2] + line[-1]
+
+            # record dollar sign in $var by a safe substitute
+            line = line.replace('\\$','\001')
+
+            # substitute inline and newline math expression wrapper
+            i=0
+            while i < len(line):
+                if line[i:i+2] == '$$':
+                    line = line[:i]+doubleSub[double]+line[i+2:]
+                    double = 1 - double
+                    i += 4
+                elif line[i] == '$':
+                    line = line[:i]+inlineSub[inline]+line[i+1:]
+                    inline = 1-inline
+                    i += 4
+                else:
+                    i += 1
+
+            # mark variables with $ sign
+            line = line.replace('\001','$')
+            md_code[j] = line
+            
+        self.py_code = python_code
+        self.md_code = md_code
+        self.test_code = test_code
+        self.html_code = self.toHtml()
 
 
-Translator.toXml = toXml
+
+    def toXml(self):
+        """
+        returns the XML code that is to be pasted into EDX studio
+        """
+        html_code = self.html_code.splitlines()
+        py_code_lines = self.py_code.splitlines()
+
+        choice_list = ""
+        updated_html_code = []
+        part_id = 1
+        for line in html_code:
+            if '<p>[_choice]</p>' == line:
+                updated_html_code += ['\n', '\n']
+                for s in py_code_lines:
+                    if '=' not in s:
+                        continue
+                    strip_start_index = s.index('=')
+                    strip_s = s[:strip_start_index+1].replace(" ", "") + s[strip_start_index+1:]
+                    sol_str = 'solution'+str(part_id)+"="
+                    opt_str = 'option'+str(part_id)+"="
+                    if sol_str in strip_s:
+                        sol_index = strip_s.index(sol_str)
+                        sol = strip_s[sol_index+len(sol_str):]
+                        while sol[0] == ' ':
+                            sol = sol[1:]
+                        sol = sol.replace('\n','')
+                        sol = sol.replace('"','')
+                        sol = sol.replace("'", "")
+                    elif opt_str in strip_s:
+                        opt_index = strip_s.index(opt_str)
+                        opt = strip_s[opt_index+len(opt_str):]
+                        while opt[0] == ' ':
+                            opt = opt[1:]
+                        opt = opt.replace("\n","")
+
+                xml_code = self.__option_wrapper(opt, sol)
+                updated_html_code += xml_code
+                part_id += 1
+
+            elif '<p>[_]</p>' == line:
+                xml_code = self.__math_wrapper('solution'+str(part_id))
+                updated_html_code += xml_code
+                part_id += 1
+
+            elif '[ ]' in line or '[x]' in line:
+                # trim <p> and </p>
+                if '<p>' in line:
+                    updated_html_code += ['\n', '\n']
+                    choice_context = line[3:]
+                if '</p>' in line:
+                    end_index = line.index("</p>")
+                    choice_context = line[:end_index]
+
+                while choice_context[0] == ' ':
+                    choice_context = choice_context[1:]
+                while choice_context[-1] == ' ':
+                    choice_context = choice_context[:-1]
+
+                if '[ ]' in line:
+                    choice_insert = self.__wrong_choice_wrapper(choice_context[3:])
+                elif '[x]' in line:
+                    choice_insert = self.__correct_choice_wrapper(choice_context[3:])
+
+                choice_list += choice_insert
+
+                if '</p>' in line:
+                    updated_html_code += self.__multi_choice_wrapper(choice_list)
+                    part_id += 1
+                    choice_list = ""
+
+            else:
+                updated_html_code.append(line)
+                updated_html_code.append("\n")
+
+        updated_html_code = "".join(updated_html_code)
+        
+        """   ERROR ERROR ERROR"""
+        xml_code = self.__XML_wrapper( self.__py_wrapper(self.py_code) + updated_html_code)
+        self.xml_code = "".join(xml_code)
+
+    def translate(self):  
+        """
+        Contains all the steps of converting from imd file to xml file.
+        Includes loading imd file, converting imd code to XML code, and saving the XML code.
+        """
+        imd_code = self.read_imd()
+        self.loadImd(imd_code)
+        self.toXml()
+        self.write_xml()
 
 
-# # <span style="color:blue">Function: </span> Translate: imd --> xml
 
-# This is one big function that does all of the steps of the functions of loading, converting, and saving the imd2xml code.
 
-# In[12]:
 
-def translate(self):  
-    self.loadImd()
-    self.toXml()
-    self.write_xml()
-
-Translator.translate = translate
 
 
 # # <span style="color:blue">Function: </span> Display Html
@@ -407,11 +360,6 @@ def test(self):
 
     
 Translator.test = test
-
-
-# In[ ]:
-
-
 
 
 # # <span style="color:lime">Main Code</span> 
