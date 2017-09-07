ebebc923db8a7360881dc0dccedfef7856cb931b
yoav freund <yfreund@ucsd.edu>
2017-09-07 14:37:05 -0700

    added Zhens content reader

---
+++ edX_CourseContentReader/README.md
@@ -0,0 +1,23 @@
+## Describe edX Course Content Data
+[Detail description from edX](http://edx.readthedocs.io/projects/devdata/en/latest/internal_data_formats/course_structure.html#course-structure)
+
+This folder contains code to generate README.md to describe edX course content file exported from edX interface.
+
+### 1. Setup
+cd to current directory and run
+
+	source setup.sh
+
+(If you want this setup script to run automatically, add a line to your .bashrc)
+
+#### You need to have Python2
+
+### 2. Using makeDoc.py
+* Export your course from edX
+* Unzip the exported .tar.gz file
+* cd into the unzipped folder
+* run makeDoc.py
+
+		makeDoc.py
+	
+* The script will generate a README.md to describe your course content.
---
+++ edX_CourseContentReader/makeDoc.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from collections import defaultdict
+
+class Doc:
+    '''
+    Create a detail documentation for the course file exported from edX.
+    '''
+
+    ## Path variables
+    path = Path('.')
+    course_path = path / 'course'
+    chapter_path = path / 'chapter'
+    seq_path = path / 'sequential'
+    draft_path = path / 'drafts'
+    vert_path = draft_path / 'vertical'
+
+    ## List of all chapters
+    chapter_list = []
+
+    ## Structure of sections and units
+    problems_struct = defaultdict(list)
+
+
+    def __makeCourse(self):
+        '''
+        Create a list of chapters by reading course.xml
+        '''
+        course_path = self.course_path / 'course.xml'
+        course_txt = course_path.open().readlines()[1:]
+        for cline in course_txt:
+            if 'chapter' in cline:
+                chap_name = cline.split('"')[1]
+                self.chapter_list.append(chap_name)
+
+    def __makeStruct(self):
+        '''
+        Create a problems to units mapping
+        by reading files from folder vertical
+        '''
+        self.problems_struct = defaultdict(list)
+        for v in self.vert_path.iterdir():
+            if v.suffix != '.xml':
+                continue
+            v_txt = v.open().readlines()
+            fline = v_txt[0]
+            sec_name = fline[fline.index('+block@')+7:].split('"')[0]
+            rank = fline[fline.index('index'):].split('"')[1]
+            comp_list = [int(rank), str(v)]
+            for vline in v_txt[1:]:
+                if '<problem ' in vline:
+                    prob = vline.split('"')[1]
+                    comp_list.append(['problem',prob])
+                elif '<video ' in vline:
+                    prob = vline.split('"')[1]
+                    comp_list.append(['video', prob])
+                elif '<html ' in vline:
+                    prob = vline.split('"')[1]
+                    comp_list.append(['html', prob])
+            self.problems_struct[sec_name].append(comp_list)
+        for k in self.problems_struct:
+            sorted_struct = sorted(self.problems_struct[k], key = lambda x: x[0])
+            self.problems_struct[k] = [s[1:] for s in sorted_struct]
+
+
+    def __init__(self):
+        self.__makeCourse()
+        self.__makeStruct()
+
+
+    def describeCourse(self):
+        readme = open('README.md', 'w')
+        readme.write("###Course structure - [course/course.xml](course/course.xml)\n")
+        self.describeChapter(readme)
+        readme.close()
+
+
+    def describeChapter(self, readme):
+        '''
+        Write section information into readme
+        '''
+        for c in self.chapter_list:
+            c += '.xml'
+            cFile = self.chapter_path / c
+            chap_txt = cFile.open().readlines()
+            first_line = chap_txt[0]
+            chap_name = first_line.split('"')[1]
+            readme.write('* [Section] {0} - [{1}]({1})\n'.format(chap_name, str(cFile)))
+            seq_list = [l.split('"')[1] for l in chap_txt if "sequential" in l]
+            self.describeSequen(seq_list, readme)
+
+
+    def describeSequen(self, seq, readme):
+        '''
+        Write subsection information into readme
+        '''
+        for s in seq:
+            s_name = s + '.xml'
+            sFile = self.seq_path / s_name
+            s_txt = sFile.open().readlines()
+            fline = s_txt[0]
+            sequ_name = fline.split('"')[1]
+            readme.write('\t* [Subsection] {0} - [{1}]({1})  \n'.format(sequ_name, str(sFile)))
+            self.describeUnit(self.problems_struct[s], readme)
+
+
+    def describeUnit(self, unit, readme):
+        '''
+        Write unit information into readme
+        '''
+        for u in unit:
+            uPath = Path(u[0])
+            fline = uPath.open().readlines()[0]
+            u_name = fline.split('"')[1]
+            readme.write('\t\t* [Unit] {0} - [{1}]({1})\n'.format(u_name, u[0]))
+            self.describeProb(u[1:], readme)
+
+    
+    def describeProb(self, probs, readme):
+        '''
+        Write component information into readme
+        '''
+        for pro in probs:
+            pro_name = pro[1]+'.xml'
+            pFile = self.draft_path / pro[0] / pro_name
+            p_txt = pFile.open().readlines()
+            fline = p_txt[0]
+            p_name = fline.split('"')[1]
+            if pro[0] == 'problem':
+                readme.write('\t\t\t* [{0}] {1} - [{2}]({2})\n'.format(pro[0], p_name, str(pFile)))
+            else:
+                readme.write('\t\t\t* [{0}] - [{1}]({1})\n'.format(pro[0], str(pFile)))
+
+
+
+if __name__ == "__main__":
+    writeDoc = Doc()
+    writeDoc.describeCourse()
+
